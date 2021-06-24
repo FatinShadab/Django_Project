c9a8489294cd607722fc993db2ed11fa8fa720ab
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2021-06-24 13:22:14 +0600

    Update urls.py

--- BLOG/clean_blogs/urls.py
+++ BLOG/clean_blogs/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('',views.BlogPostList.as_view(template_name = 'index.html'), name='home'),
@@ -12,4 +15,6 @@ urlpatterns = [
     path('books/', views.books, name='books'),
     path('contact/', views.contact, name='contact'), 
     path('<slug:slug>/', views.BlogPostDetail.as_view(), name='post'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':     settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':     settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
