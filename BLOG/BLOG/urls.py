8c1fbc8d6e2f3c847e77b069d07eb6e9df655aa1
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2021-06-24 13:21:06 +0600

    Update urls.py

--- BLOG/BLOG/urls.py
+++ BLOG/BLOG/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
-
 admin.site.site_header = "MacroTech"
 admin.site.site_title = "UMSRA Admin Portal"
 admin.site.index_title = "Welcome to MacroTech"
@@ -27,6 +23,4 @@ admin.site.index_title = "Welcome to MacroTech"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clean_blogs.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':     settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':     settings.STATIC_ROOT}),
 ]
